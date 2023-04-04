10bd9e4a2c61013a1069edbe40aa74a705728806
Ersul <ersultanff12@gmail.com>
2023-04-04 12:33:07 +0600

    Включение кэширование данных

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 from . import views
 
 urlpatterns = [
-    path('', JustHome.as_view(), name='home'),
+    path('', cache_page(60)(JustHome.as_view()), name='home'),
     path('about/', about, name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
